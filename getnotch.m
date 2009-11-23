a45e7c389ac0269793a327893ef62ea28777bc48
Michael Eager <mick.eager@gmail.com>
2009-11-24 10:25:35 +1100

    TV_Notch is ready to go.  Fixed problems with AN model taking in notchs through MATAB/Octave scripts (Fs=100kHz), that are saved to TV_notch folder.

---
+++ getnotch.m
@@ -0,0 +1,27 @@
+%function getnotch()
+close all
+
+Fs = 1e5;
+notchwidth=1/4;
+notchdepth= -30; % dB
+depth= dB2ampl(notchdepth);
+fstop1=12.5e3;
+fstop2 = fstop1+notchwidth*fstop1
+sb1 = fstop1/(Fs/2)
+sb2 = fstop2/(Fs/2)
+
+f = [0 sb1 sb1 sb2 sb2   1]
+m = [1 1 depth depth 1 1]
+[b,a] = fir2(100,f,m)
+  [h, w] = freqz(b,a);
+  plot(f,m,';target response;',w/pi,abs(h),';filter response;');
+
+
+%
+function ampl = dB2ampl(dB)
+	ampl = 10^(dB/20)
+endfunction
+	
+function ampl = dBSPL2ampl(dB)
+	ampl = 2e-5*10^(dB/20)
+endfunction
---
+++ getnotchcheb.m
@@ -0,0 +1,42 @@
+close all;
+
+Fs = 100e3
+x = 2e-5*10^(100/20)*randn(1,Fs/10);
+xstep=ceil(2*Fs/1000);    # one spectral slice every 20 ms
+   window=ceil(10*Fs/1000); # 10 ms data window
+subplot(3,1,1);
+specgram(x, 2^nextpow2(window), Fs, window, window-xstep);
+
+notchwidth=1/4;
+Rs= 30; % dB
+fstop1=12.5e3;
+fstop2 = fstop1+notchwidth*fstop1		
+		
+Ws1 = fstop1/(Fs/2)
+Ws2 = fstop2/(Fs/2)
+
+Wp1 = 0
+Wp2 = 1
+Rp=0.5
+n=8	
+% Find the best order for filter
+%[n, Wc] = cheb2ord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs)
+% Band pass (Ws1<Wp1<Wp2<Ws2) or band reject (Wp1<Ws1<Ws2<Wp2)
+%filter design. Wp gives the edges of the pass band, and Ws gives
+%the edges of the stop band.
+% [n, Wc] = cheb2ord (1000/(Fs/2), 1200/(Fs/2), 0.5, Rs);
+[b, a] = cheby2(n, Rs, [Ws1, Ws2], 'stop');
+  # band reject filter with edges pi*Wl and pi*Wh radians
+%[b, a] = cheby2 (n, 29, Wc);
+subplot(3,1,2)
+ [h, w] = freqz (b, a, [], Fs);
+ plot (w, 20*log10(abs(h)), ";;");
+ ylabel("Attenuation (dB)");
+ xlabel("Frequency (Hz)");
+
+subplot(3,1,3)
+xx = filter(b,a,x);
+specgram(xx, 2^nextpow2(window), Fs, window, window-xstep);
+
+save("/home/eagerm/Work/cnstellate/TV_notch/Notch-sb12.5-0.25oct.dat", "xx");
+print("-depsc2","/home/eagerm/Work/thesis/SimpleResponsesChapter/Notch-sb12.5-width0.25oct.eps");
