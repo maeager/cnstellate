fd3beeeb03b2819783293da7f448277fc2a218f6
Michael Eager <mick.eager@gmail.com>
2010-10-20 12:21:51 +1100

    Adding bandstop and bandpass noise to Utilities, using bandpass and notchnoise in Verify.hoc.  Switching neuron over to hg in /usr/local/nrn from snapshot in HOME.

--- getnotchcheb.m
+++ getnotchcheb.m
@@ -1,42 +1,66 @@
-function getnotchcheb(Fs,fstop1,notchwidth,Rs,filename)
+function getnotchcheb(Fs,fstop1,notchwidth,Rs,filename,printflag)
 %close all;
 %Fs = 100e3
-x = 2e-5*10^(100/20)*randn(1,Fs/10);
+x = 2e-5*10^(100/20)*randn(1,Fs/10); %get 100 ms of noise from Fs=100KHz
 xstep=ceil(2*Fs/1000);    % one spectral slice every 20 ms
 window=ceil(10*Fs/1000); % 10 ms data window
-subplot(3,1,1);
-specgram(x, 2^nextpow2(window), Fs, window, window-xstep);
-n=9;
+if nargin == 6 && printflag == 1
+  subplot(3,1,1);
+  specgram(x, 2^nextpow2(window), Fs, window, window-xstep);
+end
+ 
 %notchwidth=1; n=9
 %notchwidth=1/2; n=9
 %notchwidth=1/4; n=7
 %Rs= 30; % dB
-fstop2=fstop1+notchwidth*fstop1; 
+if notchwidth > 10 
+  printf("Two stopbands mode\n")
+  if notchwidth > fstop1
+    fstop2=notchwidth; 
+  else
+    printf("Error Two stopbands mode, fstop2 not bigger than fstop1.\n")
+  end 
+else
+  fstop2=fstop1+notchwidth*fstop1; 
+end
 % fstop1 = fstop2-notchwidth*fstop2		
 Ws1 = fstop1/(Fs/2);
 Ws2 = fstop2/(Fs/2);
-Wp1 = 0;
-Wp2 = 1;
+if Ws1 < 0
+   Ws1=0;
+end
+if Ws2 > 1
+  Ws2=1;
+end
+
+Wp1 = (100+fstop1)/(Fs/2);
+Wp2 = (fstop2-10)/(Fs/2);
 Rp=0.5;
 % Find the best order for filter
 %[n, Wc] = cheb2ord([Wp1, Wp2], [Ws1, Ws2], Rp, Rs)
 % Band pass (Ws1<Wp1<Wp2<Ws2) or band reject (Wp1<Ws1<Ws2<Wp2)
 %filter design. Wp gives the edges of the pass band, and Ws gives
 %the edges of the stop band.
-% [n, Wc] = cheb2ord (1000/(Fs/2), 1200/(Fs/2), 0.5, Rs);
+% [n, Wc] = cheb2ord (1000/(Fs/2), 1200/(Fs/2), 0.5, Rs) ;
+n=14
+do
+printf("Ws1 %g  Ws2 %g\n",Ws1,Ws2)
 [b, a] = cheby2(n, Rs, [Ws1, Ws2], 'stop');
 % band reject filter with edges pi*Wl and pi*Wh radians
 %[b, a] = cheby2 (n, 29, Wc);
-subplot(3,1,2);
 [h, w] = freqz (b, a, [], Fs);
-plot (w, 20*log10(abs(h)), ";;");
-ylabel("Attenuation (dB)","fontsize",14);
-xlabel("Frequency (Hz)","fontsize",14);
-
-subplot(3,1,3)
+n-=1
+until (max(20*log10(abs(h))) < 0.5)
 notchnoise = filter(b,a,x);
-specgram(notchnoise, 2^nextpow2(window), Fs, window, window-xstep);
 
+if nargin == 6 && printflag == 1  
+  subplot(3,1,2);
+  plot (w, 20*log10(abs(h)), ";;");
+  ylabel("Attenuation (dB)","fontsize",14);
+  xlabel("Frequency (Hz)","fontsize",14);
+  subplot(3,1,3)
+  specgram(notchnoise, 2^nextpow2(window), Fs, window, window-xstep);
+end
 %save(filename, "notchnoise");
 f = fopen(filename,"w+")
 fprintf(f,"%d\n%d\n",1,length(notchnoise))
@@ -44,4 +68,8 @@ for ii=1:length(notchnoise)
     fprintf(f,"%.4f\t",notchnoise(ii))
 end
 fclose(f)
-print("-depsc2",[filename ".eps"]);
+if nargin == 6 && printflag == 1  
+  print("-depsc2",[filename ".eps"]);
+end
+
+
