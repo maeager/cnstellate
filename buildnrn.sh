85055462796e1e2b1e7ac2f006140baa73984a1e
Michael Eager <mick.eager@gmail.com>
2011-04-20 15:32:34 +1000

    Adding mode header lines to HOC files for easy editing in emacs.
    Libresample has been copied to this directory, library and include files sourced from there.
    
    build.sh changed to use simplified library inclusion with 'nrnivmodl -loadflags'

--- buildnrn.sh
+++ buildnrn.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
+
+# Force rebuilding of AN model files
 rm -f i686/an*
 rm -f i686/.libs/an*
-/usr/local/nrn/i686/bin/nrnivmodl
-(cd i686; gcc -shared .libs/flushf.o .libs/SGC_VecStim.o .libs/SGCfast.o \
-.libs/an_bruce.o \
-.libs/an_zilany_v4.o \
-.libs/ka.o .libs/klt.o .libs/rm.o .libs/rm_vect.o .libs/mod_func.o \
-  -Wl,-rpath -Wl,/usr/local/nrn/i686/lib -Wl,-rpath -Wl,/usr/local/nrn/i686/lib -L/usr/local/lib -L/usr/local/nrn/i686/lib /usr/local/nrn/i686/lib/libnrnoc.so /usr/local/nrn/i686/lib/liboc.so /usr/local/nrn/i686/lib/libmemacs.so /usr/local/nrn/i686/lib/libnrnmpi.so /usr/local/nrn/i686/lib/libscopmath.so /usr/local/nrn/i686/lib/libsparse13.so -lreadline -lncurses /usr/local/nrn/i686/lib/libnrniv.so /usr/local/nrn/i686/lib/libivoc.so /usr/local/nrn/i686/lib/libneuron_gnu.so /usr/local/nrn/i686/lib/libmeschach.so /usr/local/nrn/i686/lib/libsundials.so -lm -ldl \
-../libresample.a \
--march=atom -mtune=atom -mssse3 -mfpmath=sse  -pthread -Wl,-soname -Wl,libnrnmech.so.0 -o .libs/libnrnmech.so.0.0.0)
+
+#Check libresample
+(cd libresample-0.1.3; make)
+
+NRNIVPATH=${HOME}/src/neuron/nrnmpi/i686
+
+${NRNIVPATH}/bin/nrnivmodl -loadflags `pwd`/libresample-0.1.3/libresample.a
+
+
+# previous version recompiled the libnrnmech library with the libresample library included
+# ${NRNIVPATH}/bin/nrnivmodl &&  \
+# (cd i686; gcc -shared .libs/flushf.o .libs/SGC_VecStim.o .libs/SGCfast.o \
+#   .libs/an_bruce.o \
+#   .libs/an_zilany_v4.o \
+#   .libs/ka.o .libs/klt.o .libs/rm.o .libs/rm_vect.o .libs/mod_func.o \
+#   -Wl,-rpath -Wl,${NRNIVPATH}/lib -Wl,-rpath -Wl,${NRNIVPATH}/lib -L${NRNIVPATH}/lib \
+#   ${NRNIVPATH}/lib/libnrnoc.so ${NRNIVPATH}/lib/liboc.so ${NRNIVPATH}/lib/libmemacs.so \
+#   ${NRNIVPATH}/lib/libnrnmpi.so ${NRNIVPATH}/lib/libscopmath.so ${NRNIVPATH}/lib/libsparse13.so \
+#   -lreadline -lncurses ${NRNIVPATH}/lib/libnrniv.so ${NRNIVPATH}/lib/libivoc.so \
+#   ${NRNIVPATH}/lib/libneuron_gnu.so ${NRNIVPATH}/lib/libmeschach.so ${NRNIVPATH}/lib/libsundials.so \
+#   -lm -ldl ../libresample-0.1.3/libresample.a -march=atom -mtune=atom -mssse3 -mfpmath=sse  -pthread \
+#   -Wl,-soname -Wl,libnrnmech.so.0 -o .libs/libnrnmech.so.0.0.0)
+
