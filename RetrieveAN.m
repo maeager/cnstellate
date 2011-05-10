ccbb1b2297f6ada75823bf025e08ad34713f95fe
Michael Eager <mick.eager@gmail.com>
2011-05-10 23:41:43 +1000

    Cleaning up code.

--- RetrieveAN.m
+++ RetrieveAN.m
@@ -1,25 +1,23 @@
 
 ANfile = fopen(tmpANstr)
-    if (isopen(ANfile))
-      tmp = fscanf(ANfile,"%d\n",6);       
-sg_rate = tmp(1);
-nsize = tmp(2);
-nchannels = tmp(3);
-cflo = tmp(4);
-cfhi = tmp(5);
-species = tmp(6);
-      LSRout = zeroes(nchannels,nsize);
-      HSRout = zeroes(nchannels,nsize);
-      cf = zeroes(nchannels,1);
-      for icf = 1:nchannels
-		  if (fscanf(ANfile,"%d\n",1) == icf)
-		    cf(icf) = fscanf(ANfile,"%d\n",1) 
-	    LSRout(:,icf) = fscanf(ANfile,"%.3f\t",nsize);
-	    HSRout(:,icf) = fscanf(ANfile,"%.3f\t",nsize);
-
-        end
-     end
-     sg_tdres = 1/sg_rate
-     fclose(ANfile)
-     
+if (isopen(ANfile))
+  tmp = fscanf(ANfile,"%d\n",6);       
+  sg_rate = tmp(1);
+  nsize = tmp(2);
+  nchannels = tmp(3);
+  cflo = tmp(4);
+  cfhi = tmp(5);
+  species = tmp(6);
+  LSRout = zeroes(nchannels,nsize);
+  HSRout = zeroes(nchannels,nsize);
+  cf = zeroes(nchannels,1);
+  for icf = 1:nchannels
+    if (fscanf(ANfile,"%d\n",1) == icf)
+      cf(icf) = fscanf(ANfile,"%d\n",1) 
+      LSRout(:,icf) = fscanf(ANfile,"%.3f\t",nsize);
+      HSRout(:,icf) = fscanf(ANfile,"%.3f\t",nsize);
     end
+  end
+  sg_tdres = 1/sg_rate
+  fclose(ANfile)
+end
