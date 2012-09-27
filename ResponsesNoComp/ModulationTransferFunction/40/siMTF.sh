46955f9ff14aad8419e0817df25d9ea05c66bd8b
Michael Eager <mick.eager@gmail.com>
2012-09-27 20:08:43 +1000

    Adding gnuplot and shell scripts for plotting AM responses

---
+++ ResponsesNoComp/ModulationTransferFunction/40/siMTF.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# siMTF.sh: Reorganise temporal information from vsSPIKES data files and plot using gnuplot
+#
+# Michael Eager
+
+set -eu
+
+ls [0-9]*| grep :|sed 's/://'| sort -n> freq.dat
+
+
+for cell in `seq 0 1 3`; do
+#    awk '! /#/ {print $1}' ./50/vsSPIKES.$cell.dat > vsSPIKES.$cell.dat    
+#    awk '! /#/ {print $1}' ./50/vs.$cell.dat > vs.$cell.dat
+    > vsSPIKES.$cell.dat;> vs.$cell.dat
+    {
+    for i in `ls  */vsSPIKES.$cell.dat|sort -n`
+    do 
+	   grep '^50' $i | awk '{print $2}'
+	   awk '! /#/ {print $2}' $i > /tmp/vsSPIKES.dat
+        cp vsSPIKES.$cell.dat /tmp/vsSPIKES.$cell.dat
+	   paste /tmp/vsSPIKES.$cell.dat /tmp/vsSPIKES.dat > vsSPIKES.$cell.dat
+    done 
+    for i in `ls  */vs.$cell.dat|sort -n`
+    do 
+	   awk '! /#/ {print $2}' $i > /tmp/vs.dat
+        cp vs.$cell.dat /tmp/vs.$cell.dat
+	   paste /tmp/vs.$cell.dat /tmp/vs.dat > vs.$cell.dat
+    done
+    } > /tmp/vsSPIKES5810.dat
+    paste freq.dat /tmp/vsSPIKES5810.dat > vsSPIKES_5810.$cell.dat
+    rm -f /tmp/vsSPIKES* /tmp/vs.*
+    sed -e 's/CELL/'$cell'/' -e 's/vs_5810/vsSPIKES_5810/' tMTF.gnu | gnuplot
+    sed -e 's/CELL/'$cell'/' tMTFmap.gnu | gnuplot
+    display tMTF_spikes.$cell.eps &
+    display tMTF5810_spikes.$cell.eps &
+done
+
+for cell in `seq 0 1 3`; do
+    {
+    for i in `ls  */spctVS.$cell.dat|sort -n`
+    do
+        fm=${i%%/*t}
+	   awk '! /#/ {x=$3/$2; print $1,'$fm',x}' $i 
+        echo ""
+    done
+    } > spctVS.$cell.dat
+    grep '^50' spctVS.$cell.dat | awk '{print $2,$3}' > spctVS_5810.$cell.dat
+    sed -e 's/CELL/'$cell'/' \
+        -e 's/DATAFILE/spctVS_5810/' \
+        -e 's/EPSFILE/siMTF5810/' tMTF.gnu | gnuplot
+    sed -e 's/CELL/'$cell'/' \
+        -e 's/DATAFILE/spctVS/' \
+        -e 's/EPSFILE/siMTF/' tMTFmap.gnu | gnuplot
+    display siMTF5180.$cell.eps &
+    display siMTF.$cell.eps &
+done
+
+# 
+# 
+# 
+# 
+# touch TS_vs.dat
+# {
+# for i in `ls  */vs.0.dat|sort -n`
+# do 
+# 	grep '^50' $i | awk '{print $2}'
+# 	awk '{print $2}' $i > /tmp/vs.dat
+# 	paste TS_vs.dat /tmp/vs.dat > TS_vs.dat
+# done
+# } > tstellate.dat
+# paste freq.dat tstellate.dat > TS_vs_5810.dat
+# rm -f tstellate.dat
+# gnuplot TS_temporal.gnu
+# 
+# 
+# touch DS_vs.dat
+# {
+# for i in `ls */vs.2.dat|sort -n`
+# do 
+# 	grep '^50' $i | awk '{print $2}'
+# 	awk '{print $2}' $i > /tmp/vs.dat
+# 	paste DS_vs.dat /tmp/vs.dat > DS_vs.dat
+# done
+# } > dstellate.dat
+# paste freq.dat dstellate.dat > DS_vs_5810.dat
+# rm -f dstellate.dat
+# sed 's/TS/DS/' TS_temporal.gnu | gnuplot
+# 
+# touch TV_vs.dat
+# {
+# for i in `ls */vs.1.dat|sort -n`; 
+# do 
+#     grep '^50' $i | awk '{print $2}'; done
+# 	awk '{print $2}' $i > /tmp/vs.dat
+# 	paste TV_vs.dat /tmp/vs.dat > TV_vs.dat
+# }> tuberculo.dat
+# paste freq.dat tuberculo.dat > TV_vs_5810.dat
+# rm -f tuberculo.dat
+# sed 's/TS/TV/' TS_temporal.gnu | gnuplot
+# 
+# touch G_vs.dat
+# {
+# for i in `ls */vs.3.dat|sort -n`
+# do 
+# 	grep '^50' $i | awk '{print $2}'
+# 	awk '{print $2}' $i > /tmp/vs.dat
+# 	paste G_vs.dat /tmp/vs.dat > G_vs.dat
+# done
+# } > golgi.dat
+# paste freq.dat golgi.dat > G_vs_5810.dat
+# rm -f golgi.dat
+# sed 's/TS/G/' TS_temporal.gnu | gnuplot
+# 
+# 
+# 
+# display TS_tMTF.eps &
+# display DS_tMTF.eps &
+# display TV_tMTF.eps &
+# display G_tMTF.eps &
+
+
+#rm -f *.dat
---
+++ ResponsesNoComp/ModulationTransferFunction/60/ratetemporal.gnu
@@ -0,0 +1,44 @@
+
+set terminal postscript eps enhanced defaultplex \
+   leveldefault color colortext \
+   solid dashlength 1.0 linewidth 2.0 butt noclip \
+   palfuncparam 2000,0.003 \
+   "Helvetica" 18 size 5,3
+   
+set output "ratetemporal.2.eps"
+
+#set term pngcairo enhanced color solid \
+#    dashlength 1.0 linewidth 2.0 butt \
+#    transparent crop font "Helvetica,18" size 7cm,5cm
+#set output "ratetemporal.2.png"
+
+
+#set yrange [0:90]
+set xlabel "f_m (Hz)" font "Helvetica,24"
+set ylabel "Channel Position" font "Helvetica,24"
+
+set pm3d map
+#set logscale x 10
+set colorbox noborder
+set multiplot layout 1,2
+set xtics out ( "100" 100, "" 200, "300" 300, "" 400, "500" 500, "" 600, "700" 700, "" 800)
+
+#set logscale y 10
+set cbrange [0:*]
+#set palette model RGB
+#set palette defined
+#set palette defined (0 "blue", 150 "white", 300 "red")
+set palette rgbformulae 22,13,-31
+splot [50:800][0:99] 'response_area.2.dat' u 1:2:($4*5)
+#unset palette
+unset ylabel
+unset logscale y
+set cbrange [0:1]
+#set palette model HSV rgbformulae 3,2,2
+#set palette model XYZ rgbformulae 7,5,15
+#set palette defined ( 0 0 0 0, 1 1 1 1 )
+set palette rgbformulae 7,5,15
+splot [50:800][0:99] 'vsSPIKES.2.dat' matrix u ($1*50+50):2:3
+unset multiplot
+!fixbb ratetemporal.2.eps
+
