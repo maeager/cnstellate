822a4d649402995192306a015b769dba7ceaa3e7
Michael Eager <mick.eager@gmail.com>
2012-10-21 14:13:39 +1100

    Remove Responses/RateLevel data files generated from older models and parameters.

---
+++ test1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ARCH=`arch`
+NRNHOME="$HOME/nrn-6.1/nrnmpi"
+NRNBIN="$HOME/$ARCH/bin/"
+NRNIV="${NRNBIN}nrniv"
+LNRNMECH=`pwd`/${ARCH}/.libs/libnrnmech.so"
+MPIBIN="/opt/mpich/ch-p4/bin/"
+MPIRUN="${MPIBIN}mpirun"
+
+if [[ `ps -u $USER | grep ssh-agent` == "" ]]
+then
+    nohup ssh-agent -s > ~/.ssh-agent
+fi
+source ~/.ssh-agent
+ssh-add -t 900000
+
+dorun() {
+        echo "starting dorun $np"
+        "${MPIRUN}" -np $1 "${NRNIV}" -dll "${LNRNMECH}" par_batch1.hoc >& temp
+        rm -r -f result.$1
+        mkdir result.$1
+        cp vowel* result.$1
+        sort -k 1n,1n -k 2n,2n out.dat > out.$1
+        mv temp stdout.$1
+        echo "completed dorun $np"
+}
+
+
+for np in 25 20 15;
+dorun $np
+done
