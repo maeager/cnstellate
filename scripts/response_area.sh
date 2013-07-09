4e87c181985abab2a71f052d028e262dc1d633ba
Michael Eager <mick.eager@gmail.com>
2013-07-09 12:15:57 +1000

    Allow for '-' in ls and grep file listing

--- scripts/response_area.sh
+++ scripts/response_area.sh
@@ -5,11 +5,11 @@ rm -f response_area.[0-3].dat
 
 for cell in $(seq 0 1 3); do
 {
-for i in `ls */rateplace.$cell.dat|sort -n`
+for i in `ls -- */rateplace.$cell.dat|sort -n`
 do 
         spl=`echo $i| sed 's/\(.*\)\/.*/\1/'`
 #	echo $spl
-	grep '^[0-9]' $i | awk '{print '$spl',$1,$2,$3}'
+	grep '^[0-9]' -- $i | awk '{print '$spl',$1,$2,$3}'
 	echo ""
 done
 }>> response_area.$cell.dat
