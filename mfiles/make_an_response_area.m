6934f76ca9cb064e9572936020f22fac2e758ff5
Michael Eager <mick.eager@gmail.com>
2013-08-12 16:31:25 +1000

    Merging MASSIVE with MBP2

--- mfiles/make_an_response_area.m
+++ mfiles/make_an_response_area.m
@@ -15,34 +15,33 @@ hsrfile = fopen("response_area.4.dat","w+");
 lsrfile = fopen("response_area.5.dat","w+");
 [status lsfiles] = system("find -maxdepth 1 -type d | grep -v -e '^.$' | tr -d './' |sort -n");
 lsfiles = regexp(lsfiles,'\n','split');lsfiles(end)=[];
-
-num_freqmod= numel(lsfiles);
+num_dir= numel(lsfiles);
 tic
 
-for ii = 1:num_freqmod
-  freqmod = str2num(lsfiles{1,ii})
+for ii = 1:num_dir
+  dir_id = str2num(lsfiles{1,ii})
   hsrras = load([lsfiles{1,ii} "/anHSR_raster.dat"] );
   lsrras = load([lsfiles{1,ii} "/anLSR_raster.dat"] );
-  num_fibres = hsrras(end,2)+1;
-  tstop = 162; % ceil(max(hsrras(:,4)));  % good chance the HSR units occur in last timestep
-  duration = 140;%tstop - onset; % in ms 
-  freqmod
-      for jj=0:99
-         %% Rate 
-      xh = hist(hsrras(find(hsrras(:,1)==jj),4),1:tstop);    
-      fprintf(hsrfile,"%d\t%d\t%d\t%.3f\n",freqmod,jj,cf(jj,cflo,cfhi),mean(xh(20:end))/(num_fibres*duration/1000));
-      %% LSR
-      if numel(lsrras(find(lsrras(:,1)==jj)))>2
-	xh = hist(lsrras(find(lsrras(:,1)==jj),4),1:tstop);
-	%      LSR(ii,jj+1) = mean(xh(20:end-1))*100/5;
-	fprintf(lsrfile,"%d\t%d\t%d\t%.3f\n",freqmod,jj,cf(jj,cflo,cfhi),mean(xh(20:end))/(num_fibres*duration/1000));
-      else 
-	fprintf(lsrfile,"%d\t%d\t%d\t%g\n",freqmod,jj,cf(jj,cflo,cfhi),0.0);
-      end
-            
-      end
-      fprintf(hsrfile,"\n");
-      fprintf(lsrfile,"\n");
+  num_hsr_fibres =   max(hsrras(:,2));
+  num_lsr_fibres = max(lsrras(:,2));
+  tstop = ceil(max(hsrras(:,4)));  % good chance the HSR units occur in last timestep, if not the max val
+
+  dir_id
+  for jj=0:99
+    %% Rate 
+    xh = hist(hsrras(find(hsrras(:,1)==jj),4),1:1:tstop);    
+    fprintf(hsrfile,"%d\t%d\t%d\t%.3f\n",dir_id,jj,cf(jj,cflo,cfhi),mean(xh(20:end))/(num_hsr_fibres/1000));
+    %% LSR
+    if numel(lsrras(find(lsrras(:,1)==jj)))>2
+      xh = hist(lsrras(find(lsrras(:,1)==jj),4),1:1:tstop);
+      fprintf(lsrfile,"%d\t%d\t%d\t%.3f\n",dir_id,jj,cf(jj,cflo,cfhi),mean(xh(20:end))/(num_lsr_fibres/1000));
+    else 
+      fprintf(lsrfile,"%d\t%d\t%d\t%g\n",dir_id,jj,cf(jj,cflo,cfhi),0.0);
+    end
+    
+    end
+    fprintf(hsrfile,"\n");
+    fprintf(lsrfile,"\n");
 end
 
 fclose(hsrfile);
---
+++ scripts/fullbattery.sh
@@ -0,0 +1,13 @@
+ cd RateLevel/
+ ../../scripts/get_response_area_first_level.sh
+ cd ../NoiseRateLevel/
+ ../../scripts/get_response_area_first_level.sh 
+ cd ../MaskedRateLevel/
+ ../../scripts/get_response_area_first_level.sh 
+ cd ../MaskedResponseCurve/
+ ../../scripts/get_response_area_first_level.sh 
+ cd ../F0Response/
+ ../../scripts/first_level_si_rate.sh 
+ cd ../ModulationTransferFunction/
+ ../../scripts/second_level_si_rate.sh 
+
