7678b03621a67a5ffb99cdbdebd63b25155d457c
Michael Eager <michael.eager@monash.edu>
2013-08-12 16:22:45 +1000

    Remove freqmod tag from make_an_response_area.m and replace with dir_id. Fix rate output by getting number of HSR and LSR fibres. Remove duration and tstop hardcoded variables.

--- scripts/get_response_area_first_level.sh
+++ scripts/get_response_area_first_level.sh
@@ -2,7 +2,6 @@
 
 ResponsesScriptsPath=$(dirname $0)
 ResponsesMPath=${ResponsesScriptsPath}/../mfiles
-# cd  /media/c4bb64a6-7c5f-4dc1-9965-b0f4c1117b36/Work-archive/cnstellate-2.58/TStellate2_CS/ModulationTransferFunction/
 
 ${ResponsesScriptsPath}/response_area.sh;
 nice octave -f -q  --eval "tic;addpath('"$ResponsesMPath"');make_an_response_area(200,40000);toc;" 
