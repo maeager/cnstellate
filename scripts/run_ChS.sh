eed1c5a0cf1b2c8f2016b2c95bc5401768973277
Michael Eager <michael.eager@monash.edu>
2013-08-27 21:46:28 +1000

    Adding TS result files and pbs script for running verification and AM responses procedures.

---
+++ scripts/run_ChS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -N run_ChS
+#PBS -l walltime=24:0:0
+#PBS -l pmem=8000MB
+#PBS -A Monash038
+# PBS -m ae
+
+
+cd $PBS_O_WORKDIR
+# cd `dirname $0`/..
+module load octave/3.6.2
+
+
+# ./mpi/special AMResponses.hoc -c "SimpleResponses_CS()" -c "AMresponse_CS()"
+./mpi/special AMResponses.hoc -c "AMresponse_CS()"
+
+cd TStellate2_CS
+../scripts/fullbattery.sh
