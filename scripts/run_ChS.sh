28547df0db5ca6ed5b44103b0323ee7165348c13
Michael Eager <michael.eager@monash.edu>
2013-11-07 16:31:27 +1100

    Update run_CS.sh batch file

--- scripts/run_ChS.sh
+++ scripts/run_ChS.sh
@@ -2,17 +2,17 @@
 #PBS -N run_ChS
 #PBS -l walltime=24:0:0
 #PBS -l pmem=8000MB
-#PBS -A Monash038
+#PBS -A Monash016
 # PBS -m ae
 
 
-cd $PBS_O_WORKDIR
-# cd `dirname $0`/..
-module load octave/3.6.2
+cd "${PBS_O_WORKDIR}"
+
+module load gnuplot octave/3.6.2
 
 ./mpi/special TStellate2.hoc -c "optimise_CS()"
 # ./mpi/special AMResponses.hoc -c "SimpleResponses_CS()" -c "AMresponse_CS()"
 # ./mpi/special AMResponses.hoc -c "AMresponse_CS()"
 
-cd TStellate2_CS
-../scripts/fullbattery.sh
+# cd TStellate2_CS
+# ../scripts/fullbattery.sh
